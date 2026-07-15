14a8d8d5b2e3d113045cbc44f86b1d96ceaed76f
Vishal Gupta <ims.vishal@gmail.com>
2026-07-16 02:27:20 +0530

    Changed firebase configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAe3JXG_zrceZjVXHP-VcSJcB69KAspxIE",
+  authDomain: "pickaso-ab898.firebaseapp.com",
+  projectId: "pickaso-ab898",
+  storageBucket: "pickaso-ab898.firebasestorage.app",
+  messagingSenderId: "96625687566",
+  appId: "1:96625687566:web:ab1827732c09ce8345105f",
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
